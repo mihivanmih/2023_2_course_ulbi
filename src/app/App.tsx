4f550823d9451429705d116640c0df0adad52c77
Gusb <mihivanmih@gmail.com>
2023-02-03 23:42:16 +0300

    9-10 lesson - Feature-Sliced

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,11 @@
-import {Suspense, useContext, useState} from "react";
+import {Suspense} from "react";
 import {Link} from "react-router-dom";
 import {Route, Routes} from "react-router-dom";
-import {Counter} from "./components/counter";
 import './styles/index.scss'
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
+import {classNames} from "shared/lib/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
 
 
 const App = () => {
@@ -23,12 +22,10 @@ const App = () => {
 
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync/>}/>
-                    <Route path={'/'} element={<MainPageAsync/>}/>
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                    <Route path={'/'} element={<MainPage/>}/>
                 </Routes>
             </Suspense>
-
-            <Counter/>
         </div>
     );
 };
