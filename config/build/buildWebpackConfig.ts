b2b60a6d6a225c1ab46c62f8ed498de75983dcd3
Gusb <mihivanmih@gmail.com>
2023-02-03 16:55:33 +0300

    5 lesson - css modules

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,6 +1,5 @@
 import {BuildOptions} from "./types/config";
 import webpack from "webpack";
-import path from "path";
 import {buildPlugins} from "./buildPlugins";
 import {buildLoaders} from "./buildLoaders";
 import {buildResolvers} from "./buildResolvers";
@@ -20,7 +19,7 @@ export function buildWebpackConfig(options: BuildOptions):webpack.Configuration 
         },
         plugins: buildPlugins(options),
         module: {
-            rules: buildLoaders()
+            rules: buildLoaders(options)
         },
         resolve: buildResolvers(),
         devtool: isDev ? 'inline-source-map' : undefined,
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import {Counter} from "./components/counter";
+import './index.scss'
+
+const App = () => {
+    return (
+        <div className={"app"}>
+            Hello
+            <Counter />
+        </div>
+    );
+};
+
+export default App;
