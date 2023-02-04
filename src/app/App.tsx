b300c48acfd27f079d4192c2e9241c384ddef581
Gusb <mihivanmih@gmail.com>
2023-02-04 23:43:47 +0300

    12 lesson - Navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,9 @@
-import {Suspense} from "react";
 import {Link} from "react-router-dom";
-import {Route, Routes} from "react-router-dom";
 import './styles/index.scss'
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
 import {AppRuter} from "app/providers/router";
+import {Navbar} from "widgets/Navabr";
 
 
 const App = () => {
@@ -15,12 +12,8 @@ const App = () => {
 
     return (
         <div className={classNames('app', {}, [theme])}>
-
+            <Navbar />
             <button onClick={toggleTheme}>TOGGLE</button>
-
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
-
             <AppRuter />
         </div>
     );
