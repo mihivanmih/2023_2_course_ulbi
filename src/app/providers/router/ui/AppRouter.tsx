0b37c0ca4bd1ee817fd22710409b610736b5f13b
Gusb <mihivanmih@gmail.com>
2023-02-09 21:10:07 +0300

    23 lesson - BundleAnalyzer

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,4 +1,5 @@
-import { type FC, Suspense } from 'react'
+import type { FC } from 'react'
+import { Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { routeConfig } from 'shared/config/routeConfig/routeConfig'
 import { PageLoader } from 'widgets/PageLoader'
