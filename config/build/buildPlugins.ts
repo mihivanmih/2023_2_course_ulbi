35cec371eeb77511ebeed4cb086d2f955ff3fbe8
Gusb <mihivanmih@gmail.com>
2023-02-17 13:06:19 +0300

    27 lesson - fix BundleAnalyzerPlugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -6,8 +6,7 @@ import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
 export function buildPlugins (options: BuildOptions): webpack.WebpackPluginInstance[] {
     const { paths, isDev } = options
-
-    return [
+    const plugins = [
         new HtmlWebpackPlugin({
             template: paths.build
         }),
@@ -18,10 +17,16 @@ export function buildPlugins (options: BuildOptions): webpack.WebpackPluginInsta
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev)
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false
         })
+
     ]
+
+    if (isDev) {
+        plugins.push(new webpack.HotModuleReplacementPlugin())
+        plugins.push(new BundleAnalyzerPlugin({
+            openAnalyzer: false
+        }))
+    }
+
+    return plugins
 }
